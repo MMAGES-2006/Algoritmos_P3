24389a8d4913530bff1b1f9eae23c8012bd78001
Camila Uribe <cjuribe.parra07@gmail.com>
2025-11-10 17:24:23 -0600

    main lol

--- main.cpp
+++ main.cpp
@@ -22,5 +22,42 @@ Carro::~Carro()
 
 int main()
 {
-    cout << "Holaaa :(" << endl;
+    int ingresarAuto;
+
+    cout << "Bienvenidos a UParking!" << endl
+    cout << "Para usar el estacionamiento, escribe 1. Si no deseas hacerlo, escribe 0: " << endl;
+
+    cin >> ingresarAuto;
+    if (ingresarAuto == 0);
+      {
+        cout << "Gracias! Vuelva pronto." << endl;
+
+        return 0;
+      }
+    else if (ingresarAuto == 1);
+      {
+        cout << "Cuentanos los detalles del automovil a ingresar."<< endl;
+        cout << "Su tamaño es grande o pequeño?" << endl;
+        //cin 
+        cout << "Cual es la marca de su auto?" << endl;
+        //cin
+        cout << "De que color es su automovil?" << endl;
+        //cin
+        cout << "Por ultimo, de que año es el vehiculo?" << endl;
+
+
+
+
+
+        if ( == 'g' ||  == 'G')
+        {
+
+        }
+       
+
+      }
+  
+ 
+
+
 }
