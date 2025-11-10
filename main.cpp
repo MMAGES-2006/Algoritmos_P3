71790bf4dd3de93790a6cf0f165d27fcef8d62d7
MMAGES2006 <22170@portalcedi.edu.mx>
2025-11-10 09:33:25 -0600

    Main

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Carro
+{
+private:
+    
+public:
+    Carro(/* args */);
+    ~Carro();
+};
+
+Carro::Carro(/* args */)
+{
+}
+
+Carro::~Carro()
+{
+}
+
+
+int main()
+{
+    cout << "Holaaa :(" << endl;
+}
