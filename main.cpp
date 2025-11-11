78ffb6ed66338bccbdc5aa9f8c6de6bf135f885a
MMAGES2006 <22170@portalcedi.edu.mx>
2025-11-11 09:32:10 -0600

    Class

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Carro
 {
 private:
+    string marca;
+    char tamaño;
+    string color;
+    int año;
     
 public:
-    Carro(/* args */);
+    Carro(char tamaño, string marca, string color, int año);
+
     ~Carro();
+
+    void meterCarro();
+
 };
 
-Carro::Carro(/* args */)
+Carro::Carro(char tamaño, string marca, string color, int año)
 {
+
 }
 
 Carro::~Carro()
@@ -23,4 +33,7 @@ Carro::~Carro()
 int main()
 {
     cout << "Holaaa :(" << endl;
+
+
+
 }
